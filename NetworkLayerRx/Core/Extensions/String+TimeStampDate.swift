804bef7f264f6f1d1e18696bf0712c176ad6c460
Carles Grau <carles.grau@ubeeqo.com>
2020-08-13 11:03:50 +0200

    edits

--- NetworkLayerRx/Core/Extensions/String+TimeStampDate.swift
+++ NetworkLayerRx/Core/Extensions/String+TimeStampDate.swift
@@ -1,29 +1,6 @@
-//
-//  String+TimeStampDate.swift
-//  NetworkLayerRx
-//
-//  Created by Carles Grau on 21/04/2020.
-//  Copyright © 2020 Carles Grau. All rights reserved.
-//
-
 import Foundation
 
 extension String {
-  func parseDuration(_ timeString: String) -> TimeInterval {
-      guard !timeString.isEmpty else {
-          return 0
-      }
-
-      var interval:Double = 0
-
-      let parts = timeString.components(separatedBy: ":")
-      for (index, part) in parts.reversed().enumerated() {
-          interval += (Double(part) ?? 0) * pow(Double(60), Double(index))
-      }
-
-      return interval
-  }
-  
   func mapToDate() -> String? {
     let time = parseDuration(self)
     let date = Date(timeIntervalSince1970: time)
@@ -32,5 +9,15 @@ extension String {
     dateFormatter.locale = NSLocale.current
     dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
     return dateFormatter.string(from: date)
+  }
+  
+  private func parseDuration(_ timeString: String) -> TimeInterval {
+    guard !timeString.isEmpty else { return 0 }
+    var interval: Double = 0
+    let parts = timeString.components(separatedBy: ":")
+    for (index, part) in parts.reversed().enumerated() {
+      interval += (Double(part) ?? 0) * pow(Double(60), Double(index))
+    }
+    return interval
   }
 }
